14b74438b0195c1aac85c3912342c853890b0796
Hamzaali7 <Hamzaali7@yahoo.com>
2022-10-20 13:19:43 -0400

    Click event added to grid items

---
+++ script.js
@@ -0,0 +1,38 @@
+const container = document.querySelector(".grid-container");
+const box1 = document.querySelector(".item-1");
+const box2 = document.querySelector(".item-2");
+const box3 = document.querySelector(".item-3");
+const box4 = document.querySelector(".item-4");
+const box5 = document.querySelector(".item-5");
+const box6 = document.querySelector(".item-6");
+const box7 = document.querySelector(".item-7");
+const box8 = document.querySelector(".item-8");
+const box9 = document.querySelector(".item-9");
+
+box1.addEventListener("click", e => {
+    console.log("It worked1!")
+});
+box2.addEventListener("click", e => {
+    console.log("It worked2!")
+});
+box3.addEventListener("click", e => {
+    console.log("It worked3!")
+});
+box4.addEventListener("click", e => {
+    console.log("It worked4!")
+});
+box5.addEventListener("click", e => {
+    console.log("It worked5!")
+});
+box6.addEventListener("click", e => {
+    console.log("It worked6!")
+});
+box7.addEventListener("click", e => {
+    console.log("It worked7!")
+});
+box8.addEventListener("click", e => {
+    console.log("It worked8!")
+});
+box9.addEventListener("click", e => {
+    console.log("It worked9!")
+});
